51625fc4cb3aa8776fad1e6579a9ab0275993331
Manash-%%%% <manashjbarai@gmail.com>
2025-08-09 19:04:14 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ['dmpsza32x691.cloudfront.net'],
-      remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: 'sportzpoint-media.s3.ap-south-1.amazonaws.com',
-              port: '',
-              pathname: '/**',
-          }
-      ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*', // Moved from domains
+        port: '',
+        pathname: '/**',
+      }
+    ],
   },
 };
 
