897641fa88d043e45e9dbfb89638e48342981159
Manash-%%%% <manashjbarai@gmail.com>
2025-08-09 19:09:20 +0530

    sdaczccwd

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*', // Moved from domains
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
+  // No image configuration needed
 };
 
 export default nextConfig;
